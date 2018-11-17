384f2597552f5eeefab505edffa745fef26b9b86
NGD <jch5201@naver.com>
2018-11-17 15:04:48 +0900

    create notificationManager

---
+++ scheduler/scheduler/NotificationManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tulpep.NotificationWindow;
+
+namespace scheduler
+{
+    class NotificationManager
+    {
+        static void showNotificationWithMsg(string msg)
+        {
+            PopupNotifier popup = new PopupNotifier();
+            popup.TitleText = "AS";
+            popup.ContentText = msg;
+            popup.Popup();
+        }
+    }
+}
