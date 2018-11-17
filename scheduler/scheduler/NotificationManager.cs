b7538dc35779c0c092ffb97baea3bd2461cc488b
NGD <jch5201@naver.com>
2018-11-18 07:02:28 +0900

    change notification implementation

--- scheduler/scheduler/NotificationManager.cs
+++ scheduler/scheduler/NotificationManager.cs
@@ -1,20 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tulpep.NotificationWindow;
+﻿
+using System;
+using System.Windows;
+using ToastNotifications;
+using ToastNotifications.Lifetime;
+using ToastNotifications.Position;
+using ToastNotifications.Messages;
+using ToastNotifications.Messages.Core;
 
 namespace scheduler
 {
     class NotificationManager
     {
-        public void showNotificationWithMsg(string msg)
+        Notifier notifier = new Notifier(cfg =>
         {
-            PopupNotifier popup = new PopupNotifier();
-            popup.TitleText = "Annoying Scheduler";
-            popup.ContentText = msg;
-            popup.Popup();
+            cfg.PositionProvider = new PrimaryScreenPositionProvider(
+                corner: Corner.BottomRight,
+                offsetX: 10,
+                offsetY: 10);
+
+            cfg.LifetimeSupervisor = new TimeAndCountBasedLifetimeSupervisor(
+                notificationLifetime: TimeSpan.FromSeconds(3),
+                maximumNotificationCount: MaximumNotificationCount.FromCount(5));
+
+            cfg.Dispatcher = Application.Current.Dispatcher;
+
+            cfg.DisplayOptions.Width = 500;
+            cfg.DisplayOptions.TopMost = true;
+        });
+
+        
+
+        public void showNotificationWithMsg(String msg)
+        {
+            var options = new ToastNotifications.Core.MessageOptions
+            {
+                FontSize = 20, // set notification font size
+                ShowCloseButton = false, // set the option to show or hide notification close button
+                Tag = "Any object or value which might matter in callbacks",
+            };
+            notifier.ShowError(msg,options);
         }
     }
 }
